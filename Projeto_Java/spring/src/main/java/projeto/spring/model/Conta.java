f5fe51f3b13ca77da4224e01855d728d74fc77b4
enicacio <eliane.nic@gmail.com>
2021-09-27 17:40:42 -0300

    correção_idconta

--- Projeto_Java/spring/src/main/java/projeto/spring/model/Conta.java
+++ Projeto_Java/spring/src/main/java/projeto/spring/model/Conta.java
@@ -2,11 +2,12 @@ package projeto.spring.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
@@ -14,7 +15,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class Conta {
     
     @Id
-    @Column (nullable = true, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long numConta;
 
     @Column(nullable = false)
--- Projeto_Java/spring/src/main/java/projeto/spring/repository/ContaRepo.java
+++ Projeto_Java/spring/src/main/java/projeto/spring/repository/ContaRepo.java
@@ -1,7 +1,6 @@
 package projeto.spring.repository;
 
 import org.springframework.data.repository.CrudRepository;
-
 import projeto.spring.model.Conta;
 
 public interface ContaRepo extends CrudRepository<Conta, Long> {
